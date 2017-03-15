67aaf81cbbd5824ed4f600c617a16a93b2d39a9b
Alexandre Morin <alexandre.morin@maarch.org>
2017-03-15 17:47:58 +0100

    Insert elements BDD

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 var Twitter = require('twitter');
 var fs = require('fs');
+
 var http = require('http'),
-    fs = require('fs'),
     path = require('path');
 
 var server = http.createServer(function(req, response) {
@@ -21,7 +21,9 @@ var server = http.createServer(function(req, response) {
 
 });
 
-server.listen(8080);
+server.listen(8000);
+
+var MongoClient = require("mongodb").MongoClient;
 
 var client = new Twitter({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -31,17 +33,39 @@ var client = new Twitter({
 });
 
 var params = {
-    q: "%23PowerRangers",
+    q: "%23ASMMCI",
     count: 10
 };
 
+
+
+var resTweets;
 client.get('search/tweets.json', params, function (error, tweets, response) {
     if (error) console.error(error);
-    console.log(tweets);
+    resTweets = tweets;
 
     fs.writeFile('tweets.json', JSON.stringify(tweets, null, 4), function (err) {
         if (err) throw err;
         console.log('It\'s saved!');
     });
 
+    addTweet(resTweets.statuses);
 });
+
+
+function addTweet(elements) {
+	MongoClient.connect("mongodb://localhost/twitter", function(error, db) {
+    if (error) return funcCallback(error);
+
+    console.log("Connecté à la base de données 'Twitter'");
+
+    for(var i = 0; i < elements.length; i++){
+		db.collection("tweet").insert(elements[i], null, function (error, results) {
+	    if (error) throw error;
+
+	    console.log("Le document a bien été inséré"); 
+
+		});
+	}
+	});
+}
