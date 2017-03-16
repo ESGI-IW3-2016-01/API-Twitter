5fe0b1d8263923739de20ff80ae64b1f11536d7e
Antoine Cusset <acusset@clever-age.com>
2017-03-16 10:46:44 +0100

    add scheduler

--- index.js
+++ index.js
@@ -3,12 +3,15 @@ var Twitter = require('twitter');
 var fs = require('fs');
 var http = require('http');
 var path = require('path');
+var schedule = require('node-schedule');
 
 var params = {
     q: "%23ASMMCI",
-    count: 100
+    count: 100,
+    result_type: 'recent'
 };
 var resTweets;
+var maxId;
 
 var server = http.createServer(function (req, response) {
     fs.readFile('index.html', 'utf-8', function (error, data) {
@@ -22,7 +25,6 @@ var server = http.createServer(function (req, response) {
         response.end();
     });
 });
-
 server.listen(8000);
 
 var MongoClient = require("mongodb").MongoClient;
@@ -34,16 +36,20 @@ var client = new Twitter({
     access_token_secret: process.env.TWITTER_TOKEN_SECRET
 });
 
-client.get('search/tweets.json', params, function (error, tweets, response) {
-    if (error) console.error(error);
-    resTweets = tweets;
-    // fs.writeFile('tweets.json', JSON.stringify(tweets, null, 4), function (err) {
-    //     if (err) throw err;
-    //     console.log('It\'s saved!');
-    // });
-    addTweet(resTweets.statuses);
+schedule.scheduleJob('*/1 * * * *', function () {
+    console.log('tic');
+    callTwitter();
 });
 
+function callTwitter() {
+    client.get('search/tweets.json', params, function (error, tweets, response) {
+        if (error) console.error(error);
+        maxId = tweets.search_metadata.max_id;
+        params.since_id = maxId;
+        writeFile('tweets.json', tweets.statuses);
+        addTweet(tweets.statuses);
+    });
+}
 
 function addTweet(elements) {
     MongoClient.connect('mongodb://' + process.env.MONGO_HOST + ':' + process.env.MONGO_PORT + '/' + process.env.MONGO_COL, function (error, db) {
@@ -53,5 +59,12 @@ function addTweet(elements) {
                 if (error) throw error;
             });
         }
+    });
+}
+
+function writeFile(fileName, data) {
+    if (!fileName) fileName = 'tweets.json';
+    fs.writeFile(fileName, JSON.stringify(data, null, 4), function (err) {
+        if (err) throw err;
     });
 }
